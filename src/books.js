bc96f9e98a8a6e4a459692b8708b0b58ee615b05
AlishaKausheen <alishakausheen@gmail.com>
2023-10-26 20:34:12 -0700

    basic ES6

---
+++ src/books.js
@@ -0,0 +1,27 @@
+
+ export const books = [
+  {
+    author: "Joseph Murphy",
+    title: "The power of your subconscious mind",
+    img: "https://m.media-amazon.com/images/I/61k1giyuJ5L._SY425_.jpg",
+    id: 1,
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://m.media-amazon.com/images/I/91bYsX41DVL._SY425_.jpg",
+    id: 2,
+  },
+  {
+    author: "Morgan Housel",
+    title: "The Psychology Of Money",
+    img: "https://m.media-amazon.com/images/I/41r6F2LRf8L._SY445_SX342_.jpg",
+    id: 3,
+  },
+  {
+    author: "Napoleon Hill",
+    title: "THINK AND GROW RICH ",
+    img: "https://m.media-amazon.com/images/I/41DmBeq8oIL._SY445_SX342_.jpg",
+    id: 4,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,67 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { books } from "./books";
+import Book from "./Book";
 //const title = ;
 //const author = "Joseph Murphy";
 //const img = "";
-const books = [
-  {
-    author: "Joseph Murphy",
-    title: "The power of your subconscious mind",
-    img: "https://m.media-amazon.com/images/I/61k1giyuJ5L._SY425_.jpg",
-    id: 1,
-  },
-  {
-    author: "James Clear",
-    title: "Atomic Habits",
-    img: "https://m.media-amazon.com/images/I/91bYsX41DVL._SY425_.jpg",
-    id: 2,
-  },
-  {
-    author: "Morgan Housel",
-    title: "The Psychology Of Money",
-    img: "https://m.media-amazon.com/images/I/41r6F2LRf8L._SY445_SX342_.jpg",
-    id: 3,
-  },
-  {
-    author: "Napoleon Hill",
-    title: "THINK AND GROW RICH ",
-    img: "https://m.media-amazon.com/images/I/41DmBeq8oIL._SY445_SX342_.jpg",
-    id: 4,
-  },
-];
+
 function BookList() {
-  const getBook = (id) => {
-    const book = books.find((book) => 
-      book.id === id);
-    console.log(book);
-    
-  }
   return (
     <section className="bookList">
       <EventsExample/>
       {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />;
+        return <Book {...book} key={book.id}/>;
       })}
       
     </section>
   );
 }
 
-const Book = (props) => {
-  const { img, title, author,getBook ,id } = props;
-  const getSingleBook = () => {
-    getBook(id);
-  }
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title} </h2>
-      <button onClick={getSingleBook}>displayValue</button>
-      <h4>{author}</h4>
-    </article>
-  );
-};
+
 const EventsExample = () => {
   const handleFormInput = (e) => {
     console.log("handle form input");
